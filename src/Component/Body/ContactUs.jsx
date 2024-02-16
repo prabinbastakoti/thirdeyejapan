a42fb7546051802722a8da72a6335a687f38857d
Prabin Bastakoti <Prabinbastakoti1@gmail.com>
2024-02-16 16:43:54 +0545

    add today opening time

--- src/Component/Body/ContactUs.jsx
+++ src/Component/Body/ContactUs.jsx
@@ -1,6 +1,24 @@
+import { useState } from 'react';
 import HeadingTitle from '../HeadingTitle';
+import { FaCaretDown, FaCaretUp } from 'react-icons/fa';
 
 const ContactUs = () => {
+  const [openAll, setOpenAll] = useState(false);
+  const time = [
+    { day: 'Sun', duration: 'Closed' },
+    { day: 'Mon', duration: '09:00 - 17:00' },
+    { day: 'Tue', duration: '09:00 - 17:00' },
+    { day: 'Wed', duration: '09:00 - 17:00' },
+    { day: 'Thur', duration: '09:00 - 17:00' },
+    { day: 'Fri', duration: '09:00 - 17:00' },
+    { day: 'Sat', duration: 'Closed' },
+  ];
+  const today = new Date().getDay();
+
+  const showAllTimings = () => {
+    setOpenAll(!openAll);
+  };
+
   return (
     <>
       <div>
@@ -18,9 +36,55 @@ const ContactUs = () => {
           </h1>
           <div className=" pb-5 border-b">
             <h1 className="text-2xl text-center mt-10 mb-5">Hours</h1>
-            <p className="text-lg text-center text-[#5e5e5e]">
-              This part is remaining
-            </p>
+            <div className="flex flex-row justify-center items-center text-lg text-[#5e5e5e] gap-4">
+              {!openAll ? (
+                <>
+                  <p>Open today</p>
+                  <p className="border-b-2 border-[#dad2c7]">
+                    {time[today].duration}
+                  </p>
+                  <FaCaretDown
+                    className="cursor-pointer"
+                    onClick={showAllTimings}
+                  />
+                </>
+              ) : (
+                <div className="flex flex-row gap-10">
+                  <div>
+                    {time.map((item, index) => {
+                      return (
+                        <div
+                          key={index}
+                          className={`${
+                            index === today ? 'font-semibold' : 'font-normals'
+                          }`}
+                        >
+                          <p>{item.day}</p>
+                        </div>
+                      );
+                    })}
+                  </div>
+                  <div>
+                    {time.map((item, index) => {
+                      return (
+                        <div
+                          key={index}
+                          className={`${
+                            index === today ? 'font-semibold' : 'font-normals'
+                          }`}
+                        >
+                          <p>{item.duration}</p>
+                        </div>
+                      );
+                    })}
+                  </div>
+                  <FaCaretUp
+                    className="cursor-pointer"
+                    onClick={showAllTimings}
+                  />
+                </div>
+              )}
+            </div>
           </div>
           <div className="flex flex-row justify-center mt-10 pb-10 border-b">
             <button className="bg-[#d8d0c5] px-12 py-6 text-xl font-medium tracking-widest border-b-8 border-[#b1a99e]">
